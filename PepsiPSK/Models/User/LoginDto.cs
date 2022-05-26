3bfa004b4fc7aebc92987d68bbd697fdd4ba0873
DonSabotini <kemesis123@gmail.com>
2022-05-27 01:48:36 +0300

    Change login from email to username

--- PepsiPSK/Models/User/LoginDto.cs
+++ PepsiPSK/Models/User/LoginDto.cs
@@ -4,9 +4,8 @@ namespace PepsiPSK.Models.User
 {
     public class LoginDto
     {
-        [EmailAddress]
-        [Required(ErrorMessage = "Email is required!")]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Username is required!")]
+        public string Username { get; set; }
 
         [Required(ErrorMessage = "Password is required!")]
         public string Password { get; set; }
