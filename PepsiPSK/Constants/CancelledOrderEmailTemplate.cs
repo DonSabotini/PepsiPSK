5241b2add6ae5436077a4bf59444d6733ac84a2d
DonSabotini <kemesis123@gmail.com>
2022-05-27 04:33:53 +0300

    Create templates for all order states

---
+++ PepsiPSK/Constants/CancelledOrderEmailTemplate.cs
@@ -0,0 +1,26 @@
+﻿using PepsiPSK.Entities;
+using System.Net.Mail;
+using System.Text;
+
+namespace PepsiPSK.Constants
+{
+    public class CancelledOrderEmailTemplate : IEmailTemplate
+    {
+        private MailMessage message;
+        public CancelledOrderEmailTemplate(string to, Order order)
+        {
+            message = new MailMessage(EmailConfig.FROM, to);
+            string mailbody = $"Order with id: <b>{order.OrderNumber.ToString("00000")} </b> was cancelled";
+            message.Subject = "Cancelled order " + order.OrderNumber.ToString("00000");
+            message.Body = mailbody;
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+            
+        }
+
+        public MailMessage getMessage()
+        {
+            return message;
+        }
+    }
+}
